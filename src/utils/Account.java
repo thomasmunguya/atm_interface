34dd297a73cc6cabcb784c86842df3f512c40c8c
mrtryharder2 <48857366+thomasmunguya@users.noreply.github.com>
2020-03-26 02:18:47 -0700

    Created a fresh account class to facilitate account management

--- src/utils/Account.java
+++ src/utils/Account.java
@@ -1,11 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+*** This is the account class for the atm interface program.
+*** This class has the following code:
+*** a default constructor
+*** a constructor that accepts an NRC number as argument and generates a random account number and PIN(
+***     the constructor uses the methods for generating the account number and PIN)
+*** getter and setter methods for account data fields
+*** a private instance method that takes the DB column(attribute) name and account number as arguments for retrieving data from the DB 
+*** private instance methods for storing or updating the data fields to the DB(Each of these methods takes as arguments the column(attribute) name
+***     account number and returns the value just stored.
+*** a private instance method for generating a random 11-digit account number
+*** a private instance method for generating a 4-digit PIN
  */
 package utils;
 
 import java.sql.*;
+import java.util.*;
 
 /**
  *
@@ -14,81 +23,83 @@ import java.sql.*;
 public class Account {
     
     private String nrc_number;
-    private int account_number;
-    private int pin;
-    private double balance;
+    private String account_number;
+    private String generated_account_number;
+    private String pin;
+    private String balance;
     
-    //data fields for DB columns
+    //data fields for DB data
     private static final String NRC_COL = "nrc_number";
     private static final String ACC_NUMBER_COL = "account_number";
     private static final String PIN_COL = "pin";
     private static final String BALANCE_COL = "balance";
     private static final String TABLE_NAME = "account";
-    
-   private static DatabaseHandler db_handler = new DatabaseHandler();
+    private static DatabaseHandler db_handler = new DatabaseHandler();
     
     public Account() {
         
         
     }
     
-    public Account(int account_number, String nrc_number, int pin, double balance) 
-            throws ClassNotFoundException, SQLException {
+    
+    public Account(String nrc_number) 
+            throws SQLException{
         
-        storeAccountNumber(account_number);
-        storeOrUpdateNRCNumber(nrc_number, account_number);
-        storeOrUpdatePIN(pin, account_number);
-        storeOrUpdateBalance(balance, account_number);
-        
-        
+        generated_account_number = generateAccountNumber();
+        this.account_number = storeAccountNumber(generated_account_number);
+        this.nrc_number = storeOrUpdateNRCNumber(nrc_number, generated_account_number);
+        this.pin = storeOrUpdatePIN(generatePIN(), generated_account_number);
+        this.balance = storeOrUpdateBalance("0.00", generated_account_number);
+           
     }
     
-    public int getAccountNumber() throws SQLException {
+    public final String getAccountNumber() throws SQLException {
         
-        return Integer.parseInt(retrieveData(ACC_NUMBER_COL, this.account_number));
+        return this.account_number;
     }
     
-    public void setAccountNumber(int account_number) throws SQLException{
+    public final void setAccountNumber(String account_number) throws SQLException{
         
-        storeAccountNumber(account_number);
+        this.account_number = storeAccountNumber(account_number);
     }
     
-    public String getNRCNumber() throws SQLException {
+    public final String getNRCNumber() throws SQLException {
        
             
-            return retrieveData(NRC_COL, this.account_number);
+        return this.nrc_number;
             
    }
     
     public void setNRCNumber(String nrc_number) throws SQLException {
         
-        storeOrUpdateNRCNumber(nrc_number, this.account_number);
+        this.nrc_number = storeOrUpdateNRCNumber(nrc_number, this.account_number);
     }
     
-    public int getPIN() throws SQLException {
+    public final String getPIN() throws SQLException {
         
-        return Integer.parseInt(retrieveData(PIN_COL, this.account_number));
+        return this.pin;
     }
     
-    public void setPIN(int pin) throws SQLException {
+    public void setPIN(String pin) throws SQLException {
         
-        storeOrUpdatePIN(pin, this.account_number);
+        this.pin = storeOrUpdatePIN(pin, this.account_number);
     }
     
-    public int getBalance() throws SQLException {
+    public final String getBalance() throws SQLException {
         
-        return Integer.parseInt(retrieveData(BALANCE_COL, this.account_number));
+        return this.balance;
     }
     
-    public void setBalance(double balance) throws SQLException {
+    public void setBalance(String balance) throws SQLException {
         
-        storeOrUpdateBalance(balance, this.account_number);
+       this.balance = storeOrUpdateBalance(balance, this.account_number);
+        
     } 
    
    
  
-    /*retrieve the property specified by col_name by the nrc_number*/
-    public String retrieveData(String col_name, int account_number)
+    /*retrieve the property specified by col_name by the account number*/
+    private String retrieveData(String col_name, String account_number)
         throws SQLException {
         
         Connection connection;
@@ -122,7 +133,6 @@ public class Account {
         }
         catch(SQLException exception) {
             
-            exception.printStackTrace();
         }
         
         return "No match";
@@ -131,14 +141,14 @@ public class Account {
      /**store or update the account number to the database.
      * Since the account number is a primary key in the database, this method
      * first checks if the account number already exists. If it does, no action
-     * is taken otherwise it is stored into the database 
+     * is taken otherwise it is stored into the database and returned
      */
-    public void storeAccountNumber(int account_number) throws SQLException {
+    private String storeAccountNumber(String account_number) throws SQLException {
         
         try {
             
             //modify this
-            Connection connection = DatabaseHandler.createConnection();;
+            Connection connection = DatabaseHandler.createConnection();
             
             PreparedStatement statement = connection.prepareStatement
                 ("insert into " + TABLE_NAME  + " (" + ACC_NUMBER_COL + ", " + NRC_COL
@@ -148,7 +158,7 @@ public class Account {
             //if the nrc number already exists DO NOT store it
             if(retrieveData(ACC_NUMBER_COL, account_number).equals("No match")) {
                 //will be set to retrieve values from their various methods
-                statement.setInt(1, account_number );
+                statement.setString(1, account_number );
                 statement.setNull(2, Types.VARCHAR); //set the nrc number to null
                 statement.setNull(3, Types.INTEGER); //set account balance to null
                 statement.setNull(4, Types.INTEGER);
@@ -165,88 +175,104 @@ public class Account {
         }
         catch(SQLException exception) {
             
-            exception.printStackTrace();
         }
-        
-    }
-    
-    /*store or update the account's nrc to the database*/
-    public void storeOrUpdateNRCNumber(String nrc_number, int account_number) 
-            throws SQLException {
-        
-        try {
-            
-            //modify this
-            Connection connection = DatabaseHandler.createConnection();;
-            
-            Statement statement = connection.createStatement();
-            
-            statement.executeUpdate("update " + TABLE_NAME + " set " + NRC_COL
-                    + " = '" + nrc_number + "' where " + ACC_NUMBER_COL + " = " + account_number + ";");
-            
-            connection.close();
-            
-        }
-        catch(SQLException exception) {
-            
-            exception.printStackTrace();
-        }
-        
-    }
-    
-    /*store or update the account pin to the database*/
-    public void storeOrUpdatePIN(int pin, int account_number) 
-            throws SQLException {
-        
-        try {
-            
-            //modify this
-            Connection connection = DatabaseHandler.createConnection();;
-            
-            Statement statement = connection.createStatement();
-            
-            statement.executeUpdate("update " + TABLE_NAME + " set " + PIN_COL
-                    + " = " + pin + " where " + ACC_NUMBER_COL + " = " + account_number + ";");
-            
-            connection.close();
-            
-        }
-        catch(SQLException exception) {
-            
-            exception.printStackTrace();
-        }
-        
-    }
-    
-    /*store or update the account's nrc to the database*/
-    public void storeOrUpdateBalance(double balance, int account_number) 
-            throws SQLException {
-        
-        try {
-            
-            //modify this
-            Connection connection = DatabaseHandler.createConnection();;
-            
-            Statement statement = connection.createStatement();
-            
-            statement.executeUpdate("update " + TABLE_NAME + " set " + BALANCE_COL
-                    + " = " + balance + " where " + ACC_NUMBER_COL + " = " + account_number + ";");
-            
-            connection.close();
-            
-        }
-        catch(SQLException exception) {
-            
-            exception.printStackTrace();
-        }
-        
-    }
-    
-  
-    
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        
-        Account account = new Account(242424, "481570611", 2727, 47500);
        
+        return account_number;
     }
+    
+    /*store or update the account's nrc to the database and return it*/
+    private String storeOrUpdateNRCNumber(String nrc_number, String account_number) 
+            throws SQLException {
+        
+        try {
+            
+            try ( //modify this
+                    Connection connection = DatabaseHandler.createConnection()) {
+                
+                
+                Statement statement = connection.createStatement();
+                
+                statement.executeUpdate("update " + TABLE_NAME + " set " + NRC_COL
+                        + " = '" + nrc_number + "' where " + ACC_NUMBER_COL + " = " + account_number + ";");
+            }
+            
+        }
+        catch(SQLException exception) {
+            
+        }
+        
+        return nrc_number;
+    }
+    
+    /*store or update the account pin to the database and return it*/
+    private String storeOrUpdatePIN(String pin, String account_number) 
+            throws SQLException {
+        
+        try {
+            
+            try ( //modify this
+                    Connection connection = DatabaseHandler.createConnection()) {
+                
+                
+                Statement statement = connection.createStatement();
+                
+                statement.executeUpdate("update " + TABLE_NAME + " set " + PIN_COL
+                        + " = " + pin + " where " + ACC_NUMBER_COL + " = " + account_number + ";");
+            }
+            
+        }
+        catch(SQLException exception) {
+            
+        }
+        
+        return pin;
+    }
+    
+    /*store or update the account's nrc to the database and return it*/
+    private String storeOrUpdateBalance(String balance, String account_number) 
+            throws SQLException {
+        
+        try {
+            
+            try ( 
+                    Connection connection = DatabaseHandler.createConnection()) {
+                
+                Statement statement = connection.createStatement();
+                
+                statement.executeUpdate("update " + TABLE_NAME + " set " + BALANCE_COL
+                        + " = " + balance + " where " + ACC_NUMBER_COL + " = " + account_number + ";");
+            }
+            
+        }
+        catch(SQLException exception) {
+        }
+        
+        return balance;
+    }
+    
+    //This method generates a random 11-digit account number
+    private static String generateAccountNumber(){
+        
+        Random rnd = new Random();
+        String account_number = "";
+        for (int i = 0; i < 9; i++) {
+            
+            int random_number = rnd.nextInt(10);
+            account_number += Integer.toString(random_number);
+        }
+        return account_number;
+    }
+    
+    //This method generates a random 4-digit pin
+    private static String generatePIN(){
+        
+        Random rnd = new Random();
+        String pin = "";
+        for (int i = 0; i < 4; i++) {
+            int random_number = rnd.nextInt(10);
+            pin += Integer.toString(random_number);
+        }
+        return pin;
+    }
+    
 }
