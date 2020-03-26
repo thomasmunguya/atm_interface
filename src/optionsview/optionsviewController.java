378138557e9b06b74b6e6a0769d2ad173bb22dab
mrtryharder2 <48857366+thomasmunguya@users.noreply.github.com>
2020-03-26 02:20:10 -0700

    made a few changes not worth mentioning cause they are not too important and will not affect the program

--- src/optionsview/optionsviewController.java
+++ src/optionsview/optionsviewController.java
@@ -1,22 +1,32 @@
 package optionsview;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.logging.*;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class optionsviewController implements Initializable {
 
     @FXML
-    private BorderPane borderPane;
+    private AnchorPane rootAnchorPane; 
 
     @FXML
     private Label lblHello;
-
+    
+    @FXML
+    private Button btMoneyWithdraw;
+    
+    private Stage stage;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         makeFadeIn();
@@ -25,9 +35,34 @@ public class optionsviewController implements Initializable {
     private void makeFadeIn(){
         FadeTransition fade = new FadeTransition();
         fade.setDuration(Duration.millis(500));
-        fade.setNode(borderPane);
+        fade.setNode(rootAnchorPane);
         fade.setFromValue(0);
         fade.setToValue(1);
         fade.play();
+    }
+    
+    @FXML
+    private void loadWithdraw() throws IOException {
+        
+        try {
+            
+           
+            VBox main_pane = FXMLLoader.<VBox>load(getClass().getResource("/withdraw/Withdraw.fxml"));
+            main_pane.getStylesheets().add(getClass().getResource("/assets/css/withdraw.css").toExternalForm());
+            
+            Scene scene_withdraw = new Scene(main_pane, 650, 400);
+            
+            stage = (Stage) rootAnchorPane.getScene().getWindow();
+            stage.setScene(scene_withdraw);
+            stage.setTitle("Withdraw Page");
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch(IOException exception) {
+            
+            Logger.getLogger(optionsviewController.class.getName()).log(Level.SEVERE, null, exception);
+        }
+        
+        
     }
 }
