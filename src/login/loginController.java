4a9ad7e9d244bdbf993091c2c1d8c6a4528d52be
ZeRo <Ngabeni_Lozani@rocketmail.com>
2020-03-28 23:46:55 +0200

    Added minor changes to the loginController.java

--- src/login/loginController.java
+++ src/login/loginController.java
@@ -26,7 +26,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
